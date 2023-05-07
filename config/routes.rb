08c792f68a75134001c4b470c4f598bcfe3a1698
itsAndL <ayoub003lkebch@gmail.com>
2023-05-07 13:19:34 +0100

    Using single-quoted instead of double-quoted

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "blogpost/new", to: "blog_posts#new", as: :blogpost_new 
-  get "blogpost/:id", to: "blog_posts#show", as: :blog_post
-  post "blogposts", to: "blog_posts#create", as: :blog_posts
+  get 'blogpost/new', to: 'blog_posts#new', as: :blogpost_new
+  get 'blogpost/:id', to: 'blog_posts#show', as: :blog_post
+  post 'blogposts', to: 'blog_posts#create', as: :blog_posts
   # Defines the root path route ("/")
-  root "blog_posts#index"
+  root 'blog_posts#index'
 end
